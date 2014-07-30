51f7dc15194a1122130326e39c5e3c25c2da52ce
Joe Webster <joseph.scott.webster@gmail.com>
2014-07-30 13:09:15 -0400

    creates social media icons per lynda's instructions

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-var social = function ({
+var socialMedia = {
+  facebook : 'http://facebook.com/viewsource',
+  twitter: 'http://twitter.com/planetoftheweb',
+  flickr: 'http://flickr.com/planetotheweb',
+  youtube: 'http://youtube.com/planetoftheweb'
+};
 
-  var socialMedia = {
-    facebook : 'http://facebook.com/viewsource',
-    twitter: 'http://twitter.com/planetoftheweb',
-    flickr: 'http://flickr.com/planetotheweb',
-    youtube: 'http://youtube.com/planetoftheweb'
+
+var social = function() {
+
+  var output = '<ul>',
+      myList = document.getElementsByClassName("js-social-media-icons");
+
+  for ( var key in arguments[0] ) {
+    output += '<li><a href="' + socialMedia[key] + '">' +
+    '<img src="images/' + key + '.png" alt="icon for ' + key + '">' +
+    '</a></li>';
+  }
+
+  output += '</ul>';
+
+  for (var i = myList.length - 1; i >= 0; i--) {
+    myList[i].innerHTML = output;
   };
 
-
-})(socialMedia);
+}(socialMedia);
 
