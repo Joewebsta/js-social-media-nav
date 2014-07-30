66a4283ef75c00877d4fbea606d0ee4294bba754
Joe Webster <joseph.scott.webster@gmail.com>
2014-07-30 13:24:28 -0400

    attempts to create my own solution to the social media icon challenge

--- script.js
+++ script.js
@@ -8,19 +8,39 @@ var socialMedia = {
 
 var social = function() {
 
-  var output = '<ul>',
-      myList = document.getElementsByClassName("js-social-media-icons");
+  // var output = '<ul>',
+  //     myList = document.getElementsByClassName("js-social-media-icons");
+
+  // for ( var key in arguments[0] ) {
+  //   output += '<li><a href="' + socialMedia[key] + '">' +
+  //   '<img src="images/' + key + '.png" alt="icon for ' + key + '">' +
+  //   '</a></li>';
+  // }
+
+  // output += '</ul>';
+
+  var myList = document.getElementsByClassName("js-social-media-icons"),
+      ul;
 
   for ( var key in arguments[0] ) {
-    output += '<li><a href="' + socialMedia[key] + '">' +
-    '<img src="images/' + key + '.png" alt="icon for ' + key + '">' +
-    '</a></li>';
+    ul = document.createElement("ul");
+    
+    var li = document.createElement("li"),
+        a = document.createElement("a"),
+        img = document.createElement("img");
+
+    img.setAttribute("src", "images/" + key + ".png");
+
+    ul.appendChild("img");
+
+
+
+
   }
 
-  output += '</ul>';
 
   for (var i = myList.length - 1; i >= 0; i--) {
-    myList[i].innerHTML = output;
+    myList[i].innerHTML = ul;
   };
 
 }(socialMedia);
